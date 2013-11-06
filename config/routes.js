3beeb57dac03e8dfd6ef1efe3e0d817c8ad15bf3
gregory <greg2502@gmail.com>
2013-11-06 11:11:18 -0800

    improve the router

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 module.exports.draw = function(lib_path, app){
   var path = require('path'),
-      router = require(path.join(lib_path, 'router.js'))
+      Router = require(path.join(lib_path, 'router.js'))
   ;
-  router
-    .match('/')
+  new Router(app)
+    .match('/', 'properties#index')
 }
