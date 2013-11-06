1cf827d7e86b3177e96a8e0d59b8d0cf3517f806
gregory <greg2502@gmail.com>
2013-11-05 19:55:43 -0800

    improve app

--- config/application.js
+++ config/application.js
@@ -10,19 +10,28 @@ module.exports.configure = function(options){
   global.mongodb = {} //mongoose.connect('mongodb://localhost/project');
   global.logger  = logger
 
-  //require(path.join(options.paths.lib,'models.js')).autoload(options.paths.models, mongodb);
+  require(path.join(options.paths.lib,'models.js')).autoload(options.paths.models, mongodb);
   require(path.join(options.paths.lib,'controllers.js')).autoload(options.paths.controllers);
   require(path.join(options.paths.config,'routes.js')).draw(options.paths.lib, app);
 
-  //console.log(process.env.NODE_ENV)
   app.configure('development', function(){
     app.use(express.logger('dev'));
   })
 
+  function errorHandler(err, req, res, next){
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500);
+    res.render('error_template', {error: err});
+  }
   app.configure(function(){
     app.set('port', process.env.PORT || 3000);
     app.set('views', options.paths.views);
     app.set('view engine', 'jade');
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(errorHandler);
 
 
     app.use('/public/vendor', express.static(path.join(options.paths.public, 'vendor'), {maxAge: 86400000}));
